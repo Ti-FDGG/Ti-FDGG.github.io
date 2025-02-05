da3d0ac7e84b1758a19d4c61b9105b03e711a528
rin <chenmfwyyx@163.com>
2025-02-05 16:14:49 +0800

    Site updated: 2025-02-05 16:14:48

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1738734327670";const preCache = ["/images/taichi.png","/images/banner/new_ban.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1738743283057";const preCache = ["/images/taichi.png","/images/banner/new_ban.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
